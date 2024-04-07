2faccd8c11c7724b16e88a96a2becdb0f35c6508
Gorbacheb <MegaRs1@yandex.ru>
2024-04-07 22:26:11 +0400

    Добавил проект index_app, в котором сделалал стартовую страницу index.html, сделал шаблон в котором подключил стили

--- docker_hosting/settings.py
+++ docker_hosting/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "index_app.apps.IndexAppConfig"
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_LOCATION = "static"
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'docker_hosting/static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
