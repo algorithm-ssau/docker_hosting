b8a3ea2e33001b7ed17ad5cb139ccecea338e188
panfilov <na2balll@yandex.ru>
2024-04-08 19:08:41 +0400

    Создал шаблон страницы регистрации

--- index_app/views.py
+++ index_app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     """index.html"""
     return render(request, "index_app/index.html")
+
+def registration(request):
+    """registration.html"""
+    return render(request, "index_app/registration.html")
