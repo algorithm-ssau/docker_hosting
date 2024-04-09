d37a2056b88a1072d4f580b1d03c82fa75f7f579
Anna <anna.zhuk.dreamer@yandex.ru>
2024-04-10 01:02:33 +0300

    Создала формы для CustomUser для админки

--- cabinet/admin.py
+++ cabinet/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 from .models import Hosting, Container, user_rent_docker
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+    (None, {'fields': ('about_user', 'user_image')}), )
+
+    
+# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
 
 admin.site.register(Hosting) #модель стала доступна на странице администрирования
 admin.site.register(Container)
