ffc6ede6ef2cc0106ac9e667d381fd46a9616a80
Anna <anna.zhuk.dreamer@yandex.ru>
2024-05-07 14:39:38 +0300

    добавила docker python sdk и django-cron, создала cron-job которая каждый час пытаеться получить инфу с докера

--- docker_hosting/settings.py
+++ docker_hosting/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "index_app.apps.IndexAppConfig",
-    "cabinet.apps.CabinetConfig"
-    
+    "cabinet.apps.CabinetConfig",
+    "django_cron",
 ]
 AUTH_USER_MODEL = 'cabinet.CustomUser'
 MIDDLEWARE = [
@@ -51,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CRON_CLASSES = [
+    "cabinet.cron.StatsCronJob",
 ]
 
 ROOT_URLCONF = 'docker_hosting.urls'
@@ -75,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'docker_hosting.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -85,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -116,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
