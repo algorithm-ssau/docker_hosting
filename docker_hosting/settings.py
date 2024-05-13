3332fe9105f5465f10ad8a91d180b73930fa4722
Gorby <MegaRs1@yandex.ru>
2024-05-14 01:05:53 +0400

    добавил возможность запуска из-под девконтейнера, поставил внутри него редис и постгресс

--- docker_hosting/settings.py
+++ docker_hosting/settings.py
@@ -81,10 +81,19 @@ WSGI_APPLICATION = 'docker_hosting.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DBNAME'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
+        'HOST': "localhost",
+        'PORT': 5432,
     }
+
 }
 
 # Password validation
