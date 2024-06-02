50fd85c2f3de1e1ea654d2ed1bba1784a0de21c9
Gorby <MegaRs1@yandex.ru>
2024-06-02 14:58:34 +0400

    fix celery beat

--- docker_hosting/settings.py
+++ docker_hosting/settings.py
@@ -145,3 +145,10 @@ CELERY_RESULT_BACKEND = "redis://redis:6379"
 CELERY_TIMEZONE = "Europe/Samara"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULE = {
+    "get_stats": {
+        "task": "cabinet.task.get_stats",
+        "schedule": 15.0
+    }
+}
